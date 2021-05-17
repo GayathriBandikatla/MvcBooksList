19ee56e9ad08a0f1fbd31489a535c69270eef645
Bandikatla <899777@cognizant.com>
2021-05-17 21:57:17 +0530

    Add project files.

---
+++ MvcBooksList/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcBooksList.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcBooksList.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<Book> bookview;
+        public HomeController()
+        {
+            bookview = new List<Book>()
+        {
+            new Book()
+            { BookName="HarryPotter", Author="xyz", Category="a", Subcategory="b", Publisher="au", Price=100 },
+            new Book()
+            { BookName="Invisible Man", Author="xyz", Category="a", Subcategory="b", Publisher="au", Price=50 },
+            new Book()
+            {BookName="Beloved", Author="xyz", Category="a", Subcategory="b", Publisher="au", Price=120 },
+            new Book()
+            { BookName="Anna Karenina", Author="xyz", Category="a", Subcategory="b", Publisher="au", Price=80},
+            new Book()
+            { BookName="Hamlet", Author="xyz", Category="a", Subcategory="b", Publisher="au", Price=200 },
+            new Book()
+            {BookName="Pride and Prejudice", Author="xyz", Category="a", Subcategory="b", Publisher="au", Price=90 },
+
+        };
+        }
+        public ActionResult Index()
+        {
+
+            return View(bookview);
+        }
+    }
+   }
---
+++ MvcBooksList/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcBooksList.Models
+{
+    public class Book
+    {
+        public string BookName { get; set; }
+        public string Author { get; set; }
+        public string Category { get; set; }  
+        public string Subcategory { get; set; } 
+        public string Publisher { get; set; }
+        public double Price { get; set; }
+    }
+}
