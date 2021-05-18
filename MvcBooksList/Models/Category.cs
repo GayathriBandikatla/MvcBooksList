e7bcbf7d3453e235d14dcafd2c1c2666e00eccde
pr1yan5hu <yadav_priyanshu@outlook.com>
2021-05-18 20:00:22 +0530

    added CategoryController, Category class and SubCategory class

---
+++ MvcBooksList/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcBooksList.Models
+{
+    public class Category
+    {
+        public string  CategoryName { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public string AddedBy { get; set; }
+        public DateTime AddedOn { get; set; }
+        public List<SubCategory> SubCategories { get; set; }
+    }
+}
---
+++ MvcBooksList/Models/SubCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcBooksList.Models
+{
+    public class SubCategory
+    {
+      
+        public string SubCategoryName { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public string AddedBy { get; set; }
+        public DateTime AddedOn { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
