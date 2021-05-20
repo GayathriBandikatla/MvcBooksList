b98520b19031537ac3e8d4428cba927ea2bc7737
Gayathri Bandikatla <gayathricts@gmail.com>
2021-05-20 15:24:12 +0530

    Added

--- MvcBooksList/Controllers/HomeController.cs
+++ MvcBooksList/Controllers/HomeController.cs
@@ -1,40 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MvcBooksList.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace MvcBooksList.Controllers
 {
     public class HomeController : Controller
     {
-        private List<Book> bookview;
-        public HomeController()
+        //Hosted web API REST Service base url  
+        string Baseurl = "https://localhost:44382/";
+        public async Task<ActionResult> Index()
         {
-            bookview = new List<Book>()
-        {
-            new Book()
-            { BookName="HarryPotter", Author="xyz", Category="a", Subcategory="b", Publisher="au", Price=100 },
-            new Book()
-            { BookName="Invisible Man", Author="xyz", Category="a", Subcategory="b", Publisher="au", Price=50 },
-            new Book()
-            {BookName="Beloved", Author="xyz", Category="a", Subcategory="b", Publisher="au", Price=120 },
-            new Book()
-            { BookName="Anna Karenina", Author="xyz", Category="a", Subcategory="b", Publisher="au", Price=80},
-            new Book()
-            { BookName="Hamlet", Author="xyz", Category="a", Subcategory="b", Publisher="au", Price=200 },
-            new Book()
-            {BookName="Pride and Prejudice", Author="xyz", Category="a", Subcategory="b", Publisher="au", Price=90 },
+            List<Book> activebooks = new List<Book>();
 
-        };
-        }
-        public ActionResult Index()
-        {
+            using (var client = new HttpClient())
+            {
+                //Passing service base url  
+                client.BaseAddress = new Uri(Baseurl);
 
-            return View(bookview);
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("api/GetActiveBooks");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var BooksResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Employee list  
+                    activebooks = JsonConvert.DeserializeObject<List<Book>>(BooksResponse);
+
+                }
+                //returning the employee list to view  
+                return View(activebooks);
+            }
         }
     }
-   }
+}
+
