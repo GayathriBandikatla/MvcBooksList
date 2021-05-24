8879aa28aefdb60256fb75f152a21f3656be166d
pr1yan5hu <yadav_priyanshu@outlook.com>
2021-05-25 00:15:46 +0530

    Added Jwt Structure

---
+++ MvcBooksList/jwt/JWTToken.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcBooksList.jwt
+{
+    public class JWTToken
+    {
+        public string generateJwtToken(string username, string password=null)
+        {
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("TheSecurityKeyOfThisApplicaiton"));
+            SigningCredentials credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            List<Claim> claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Name, username));
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: "123",
+                audience: "456",
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: credentials);
+
+            return (new JwtSecurityTokenHandler().WriteToken(token));
+        }
+    }
+}
